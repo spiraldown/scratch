d65e8367b4800c3196cf6c606ab4e5bba4d1aff7
spiraldown <47649528+spiraldown@users.noreply.github.com>
2019-10-10 01:03:43 +0000

    added modal for contribute

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,16 +3,15 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
-
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
       flash[:error] = '<strong> Invalid entry. </strong> Entries can only be a minimum of 3 or maxmimum of 140 characters.'
     end
     redirect_to root_path
+  end
+
+  def about
   end
 
   private
