6d5c213755b4ef7d8db076b2f613aae0f42a0a82
spiraldown <47649528+spiraldown@users.noreply.github.com>
2019-10-14 01:29:26 +0000

    added CSS for alert

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -6,9 +6,9 @@ class QuotesController < ApplicationController
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
-      flash[:error] = '<strong> Invalid entry. </strong> Entries can only be a minimum of 3 or maxmimum of 140 characters.'
+      flash[:error] = '<div id = "error"><strong> Invalid Entry </strong></div> <p> Entries can only be a minimum of 3 or maxmimum of 140 characters.</p>'
     end
-    flash[:success] = '<strong> Quote accepted! </strong> Please click show me another quote to refresh'
+    flash[:success] = '<div id = "success"><strong> Quote Accepted! </strong></div> <p> Please click show me another quote to refresh.</p>'
     redirect_to root_path
   end
 
